6177df97de896cc0f5f8957745a01b020bb17622
Abishek Samuditha <abisheksamuditha552@gmail.com>
2026-07-22 23:25:50 +0530

    Develop booking history

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -32,7 +32,7 @@ export const Favorite = () => {
       </div>
     )
   }
-
+ // This component displays a list of favorite movies. It fetches the movie data from the backend and handles loading states. If there are no favorite movies, it shows a message indicating that no movies are available.
   return movies.length > 0 ? (
     <div className='relative my-40 mb-60 px-6
     md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
