5a163cbe05bb9bc318d9990b67bfeac2caaa046c
kaveenfredi36-sketch <kaveenfredi36@gmail.com>
2026-07-22 22:56:36 +0530

    Improve dashboard interface

--- src/pages/admin/AdminConsole.jsx
+++ src/pages/admin/AdminConsole.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import StaticHtmlFrame from './StaticHtmlFrame.jsx'
 import adminConsoleHtml from './index.html?raw'
 
+// AdminConsole component that renders the static HTML content of the admin console
 export default function AdminConsole() {
   return <StaticHtmlFrame title="Admin Console" html={adminConsoleHtml} />
 }
